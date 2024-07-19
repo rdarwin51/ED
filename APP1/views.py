733c6031c8bd192b5419dfd1a3999e7be4eec873
rdarwin51 <darwin.ramirezp@outlook.com>
2024-07-18 22:28:10 -0600

    Add files via upload

---
+++ APP1/views.py
@@ -0,0 +1,10 @@
+
+
+
+from re import template
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
